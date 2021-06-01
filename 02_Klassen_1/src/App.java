95a1a67b6ce74a7a9a76ec3dec5ad5c9cb06476e
SnickErzIsBack <thiemo.neumann@web.de>
2021-06-01 09:22:03 +0200

    03_-_Fields_-_Wertzuweisung_von_aussen

--- 02_Klassen_1/src/App.java
+++ 02_Klassen_1/src/App.java
@@ -2,17 +2,22 @@ public class App {
     
     public static void main(String[] args) {
         
-        Cat cat = new Cat();
+        Cat cat1 = new Cat();
 
-    System.out.println("blick von außen: " + cat);
-    cat.tellUrAddress();
+    System.out.println("blick von außen: " + cat1);
+    cat1.name = "Grizabella";
+    cat1.tellUrAddress();
+    cat1.tellUrName();
+
 
     System.out.println("-----------------------------");
 
         Cat cat2 = new Cat();
 
     System.out.println("blick von außen: " + cat2);
+    cat2.name = "Alonzo";
     cat2.tellUrAddress();
+    cat2.tellUrName();
 
     }
 }
--- 02_Klassen_1/src/Cat.java
+++ 02_Klassen_1/src/Cat.java
@@ -1,5 +1,14 @@
 public class Cat{
+    
+    // Property | Attribut | Field
+    public String name = "nobody";
+
+
     public void tellUrAddress(){
         System.out.println("blick von innen: " + this);
+    }
+
+    public void tellUrName() {
+        System.out.println(this.name);
     }
 }
