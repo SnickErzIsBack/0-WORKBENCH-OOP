e14f09785fa40bc6cfae4893d673a40b0e7b8b7e
SnickErzIsBack <thiemo.neumann@web.de>
2021-06-02 11:12:37 +0200

    01_Test+,_private,_Getter/Setter

--- 03_Encapsulation/App.java
+++ 03_Encapsulation/App.java
@@ -2,14 +2,13 @@
 public class App {
 
 	public static void main(String[] args) {
-	
-		ausgabe("hu");
-	
+		Test test = new Test();
+		ausgabe(test.getTestStr());
+		test.setTestStr("Hai");
+		ausgabe(test.getTestStr());
 	}
-	
-	
+		
 	public static void ausgabe(String outStr){
 		System.out.println(outStr);
 	}
-
 }
---
+++ 03_Encapsulation/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    
+    private String testStr = "Hallo";
+
+    public void sayHi() {
+        System.out.println("hi");
+    }
+    // Getter
+    public String getTestStr() {
+        return testStr;
+    }
+    // Setter
+    public void setTestStr(String testStr) {
+        this.testStr = testStr;
+    }
+}
