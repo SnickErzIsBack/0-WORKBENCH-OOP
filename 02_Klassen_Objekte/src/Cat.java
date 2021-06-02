b6f2a50351ec7e4264299396808c302e4b003cf1
SnickErzIsBack <thiemo.neumann@web.de>
2021-06-02 08:58:27 +0200

    09_Cat:_isFemale+,_checkCompliance()+, _f-else{}

---
+++ 02_Klassen_Objekte/src/Cat.java
@@ -0,0 +1,38 @@
+public class Cat 
+{
+    // Property | Attribut | Field
+    public String name;
+    public String furColor;
+    public int age;
+    public boolean isFemale;
+
+    public Cat(String name, String furColor, int age, boolean isFemale) {
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+        this.isFemale = isFemale;
+    }
+
+    public String tellYourAttributes(String checkStr) {
+        switch (checkStr) {
+            case "#name":
+                return this.name;
+            case "#color":
+                return this.furColor;
+            case "#age":
+                return this.checkCompliance();
+                //return String.valueOf(this.age);
+                //return Integer.toString(this.age);
+            default:
+                return "ERROR";
+        }
+    }
+
+    public String checkCompliance(){
+        if (isFemale) {
+            return "This is an inappropriate question";
+        }else{
+            return Integer.toString(this.age);}
+    }
+
+}
