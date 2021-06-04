70e86fc1473733271e8ab1cfc8836fdb38c81c65
SnickErzIsBack <thiemo.neumann@web.de>
2021-06-04 10:35:17 +0200

    04_Protection_lvl_increased

--- 04_Inheritance/App.java
+++ 04_Inheritance/App.java
@@ -3,6 +3,7 @@ public class App {
     public static void main(String[] args) {          
         
         Cat cat1 = new Cat("Grizabella", "white", 29);
+        //#region
         ausgabe(cat1.tellYourAttributes("#name"));
         ausgabe(cat1.tellYourAttributes("#color"));
 
@@ -10,16 +11,19 @@ public class App {
         ausgabe(cat1.tellYourAge("#age"));
         ausgabe(cat1.tellYourAge("#age"));
         ausgabe(cat1.tellYourAge("#age"));
-
-        System.out.println("-----------------");
         
-        Tomcat tomcat1 = new Tomcat("Alonzo", "black", 35); //Änderung des Reference Type
+        System.out.println("-----------------");
+        //#endregion
+        
+        Tomcat tomcat1 = new Tomcat("Alonzo", "black", 35);
+        //#region
         ausgabe(tomcat1.tellYourAttributes("#name"));
         ausgabe(tomcat1.tellYourAttributes("#color"));
         ausgabe(tomcat1.tellYourAttributes("#age"));
+        //#endregion
     }
 
     public static void ausgabe(String outputStr) {
       System.out.println(outputStr);  
     }
-}
+} //EoF
--- 04_Inheritance/SuperCat.java
+++ 04_Inheritance/SuperCat.java
@@ -1,17 +1,23 @@
 public class SuperCat {
-   // Property | Attribut | Field
-   private String name;
-   private String furColor;
-   protected int age;
+    // Property | Attribut | Field
+    private String name;
+    private String furColor;
+    protected int age;
    
-   public String tellYourAttributes(String checkStr) {
-    switch (checkStr) {
-        case "#name":
-            return this.name;
-        case "#color":
-            return this.furColor;
-        default:
-            return "ERROR";
+    public SuperCat(String name, String furColor, int age) {
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+    }
+
+    protected String tellYourAttributes(String checkStr) {
+        switch (checkStr) {
+            case "#name":
+                return this.name;
+            case "#color":
+                return this.furColor;
+            default:
+                return "ERROR";
         }
     }
 } //EoF
