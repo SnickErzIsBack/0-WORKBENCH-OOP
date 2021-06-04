ad1c92543c45821cb55c984505799e0acd795e30
SnickErzIsBack <thiemo.neumann@web.de>
2021-06-04 09:20:25 +0200

    02_Superclass_functional,_+PROTECTED

--- 04_Inheritance/SuperCat.java
+++ 04_Inheritance/SuperCat.java
@@ -1,3 +1,17 @@
 public class SuperCat {
-    
-}
+   // Property | Attribut | Field
+   private String name;
+   private String furColor;
+   protected int age;
+   
+   public String tellYourAttributes(String checkStr) {
+    switch (checkStr) {
+        case "#name":
+            return this.name;
+        case "#color":
+            return this.furColor;
+        default:
+            return "ERROR";
+        }
+    }
+} //EoF
--- 04_Inheritance/Tomcat.java
+++ 04_Inheritance/Tomcat.java
@@ -1,25 +1,9 @@
-public class Tomcat
-{
+public class Tomcat extends SuperCat{
     // Property | Attribut | Field
-    private String name;
-    private String furColor;
-    private int age;
-    
+    // no properties anymore
+       
     public Tomcat(String name, String furColor, int age) {
-        this.name = name;
-        this.furColor = furColor;
-        this.age = age;
-    }
-
-    public String tellYourAttributes(String checkStr) {
-        switch (checkStr) {
-            case "#name":
-                return this.name;
-            case "#color":
-                return this.furColor;
-            default:
-                return "ERROR";
-        }
+      
     }
 
     public String tellYourAge() {
