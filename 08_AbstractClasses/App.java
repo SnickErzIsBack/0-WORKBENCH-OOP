4e758730c271c113c32e6be4a9c4a72b4e592f2b
SnickErzIsBack <thiemo.neumann@web.de>
2021-06-08 12:07:52 +0200

    05_SOLUTION:_abstract_class_SHAPE

--- 08_AbstractClasses/App.java
+++ 08_AbstractClasses/App.java
@@ -1,29 +1,24 @@
 public class App {
         public static void main(String[] args) {
-       
-        Circle circle = new Circle(5.0);
-        Rectangle rectangle = new Rectangle(10,10);
-        Triangle triangle = new Triangle(10,10);
 
-        ausgabe("-----------Areas-----------");
-        ausgabe("Fläche Kreis: " + circle.area());
-        ausgabe("Fläche Rechteck: " + rectangle.area());
-        ausgabe("Fläche Dreeick: " + triangle.area());
-        ausgabe("--------Differenzen--------");
-        ausgabe("Differenz Fläche(Rechteck - Dreieck): " + areaDiff(rectangle,triangle));
-        ausgabe("Differenz Fläche(Rechteck - Kreis): " + areaDiff(rectangle,circle));
+            Circle circle = new Circle(5.0);
+            Rectangle rectangle = new Rectangle(10,10);
+            Triangle triangle = new Triangle(10,10);
+
+            ausgabe("-----------Areas-----------");
+            ausgabe("Fläche Kreis: " + circle.area());
+            ausgabe("Fläche Rechteck: " + rectangle.area());
+            ausgabe("Fläche Dreeick: " + triangle.area());
+            ausgabe("");
+            ausgabe("--------Differenzen--------");
+            ausgabe("Fläche(Rechteck - Dreieck): " + areaDiff(rectangle,triangle));
+            ausgabe("Fläche(Rechteck - Kreis): " + areaDiff(rectangle,circle));
+            ausgabe("Fläche(Kreis-Dreieck): " + areaDiff(circle,triangle));
         }
 
-        //Problem: Aufwand/Nutzen: LINEAR!!!
-
-        //Rechteck minus Dreieck
-        public static double areaDiff(Rectangle r, Triangle t){
-            return r.area() - t.area();
-        }
-
-        //Rechteck minus Kreis
-        public static double areaDiff(Rectangle r, Circle c){
-            return r.area() - c.area();
+        //Shape - Shape ...One Size fits all...
+        public static double areaDiff(Shape s1, Shape s2) {
+            return s1.area() - s2.area();
         }
 
         public static void ausgabe(String outputStr) {
