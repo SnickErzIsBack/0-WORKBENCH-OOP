4b703bebc79e0ebff991ed0d9badfa12119652f0
SnickErzIsBack <thiemo.neumann@web.de>
2021-06-09 14:36:15 +0200

    02_modified_ARRAY_LIST

--- 11_ArrayLists/App.java
+++ 11_ArrayLists/App.java
@@ -11,83 +11,87 @@ public class App {
 		
 		ArrayList<String> carMakers = new ArrayList<>();
 		
-		System.out.println(carMakers);
-		System.out.println(carMakers.size());
-//		
-//		// AL dynamisch f�llen (JS --> push() / J --> add())
-//		
-//		carMakers.add("Volvo");
-//		carMakers.add("BMW");
-//		carMakers.add("Ford");
-//		carMakers.add("Honda");
-//		carMakers.add("Tesla");
-//		
+		// System.out.println(carMakers);
+		// System.out.println(carMakers.size());
+		
+		// AL dynamisch f�llen (JS --> push() / J --> add())
+		
+		carMakers.add("Volvo");
+		carMakers.add("BMW");
+		carMakers.add("Ford");
+		carMakers.add("Honda");
+		carMakers.add("Tesla");
+		
 //		System.out.println(carMakers);
 //		System.out.println(carMakers.size());
-//		
+		
 //		// Daten aus AL lesen
 //		System.out.println(carMakers.get(0));
-//		
+		
 //		// Iteration 1
-//		for (int i = 0; i < carMakers.size(); i++) {
-//			System.out.print(carMakers.get(i) + " ");
-//		}
-//
 //		System.out.println("");
 //		System.out.println("------- Iteration 1 -----------");
-//		
+//		System.out.println("");
+
+//		for (int i = 0; i < carMakers.size(); i++) {
+//		System.out.print(carMakers.get(i) + " ");
+//		 }
+		
+//		System.out.println("");
+		
 //		// Iteration 2
-//		for (String string : carMakers) {
-//			System.out.print(string + " ");
-//		}
-//		
 //		System.out.println("");
 //		System.out.println("------- Iteration 2 ---------");
-//		
-//		// Daten in AL ersetzen / �berschreiben
+//		System.out.println("");
+
+//		for (String string : carMakers) {
+//		System.out.print(string + " ");
+//		}
+		
+		
+// 		// Daten in AL ersetzen / �berschreiben
 //		carMakers.set(1, "MB");
 //		System.out.println(carMakers);
-//		
-//		System.out.println("");
-//		System.out.println("-------- sortiert: ------------");
-//		
-//		Collections.sort(carMakers);
-//		System.out.println(carMakers);
-//		
+		
+		System.out.println("");
+		System.out.println("-------- sortiert: ------------");
+		
+		Collections.sort(carMakers);
+		System.out.println(carMakers);
+//		System.out.println(carMakers.get(0));
+		
 //		// Daten entfernen (JS --> pop(); J --> remove())
-//		
+		
 //		carMakers.remove(0);
 //		System.out.println(carMakers);
+
 //		carMakers.remove(carMakers.size()-1);
 //		System.out.println(carMakers);
-//	
+	
 //		// AL leeren ...
 //		carMakers.clear();
 //		System.out.println(carMakers);
-//		
-//		System.out.println("");
-//		System.out.println("-------Thema: Wrapper -------------");
-//		
-//		//ArrayList<String> strList = new ArrayList<>();
-//		
-//		//ArrayList<int> intList = new ArrayList<>();
-//		// L�sung: Wrapper
 		
-//		//int boo = 2;
+		System.out.println("");
+		System.out.println("-------Thema: Wrapper -------------");
+		System.out.println("");
+
+		ArrayList<String> strList = new ArrayList<>();
+		
+//		ArrayList<int> intList = new ArrayList<>();
+		// L�sung: Wrapper
+		
+//		int boo = 2;
 //		Integer boo = 2;
 //		System.out.println("Inhalt: " + boo.intValue());
-//		
-//		ArrayList<Integer> intList = new ArrayList<>();
-//		
-//		intList.add(10);
-//		intList.add(11);
-//		intList.add(12);
-//		
-//		System.out.println(intList);
 		
+		ArrayList<Integer> intList = new ArrayList<>();
 		
+		intList.add(10);
+		intList.add(11);
+		intList.add(12);
+		
+		System.out.println(intList);
 	}
-
 	
-	
-}
+}//EoF
