17f301af05f29e95d99fd47a17251dd1f9cc0444
SnickErzIsBack <thiemo.neumann@web.de>
2021-06-10 08:41:37 +0200

    03_Arrays

--- 12a_Klausurvorbereitung_PP/funktionen.js
+++ 12a_Klausurvorbereitung_PP/funktionen.js
@@ -1,12 +1,27 @@
 /* DATENSTRUKTUREN */
 
 /* >>> Primitives <<<*/
-let a; // VAR - Deklaration - untypisierte sprache
 
-a = true; // Datentyp BOOLEAN
-a = "Hallo!" // Datentyp STRING
-a = 2;  // Datentyp NUMBER
+// let a; // VAR - Deklaration - untypisierte sprache
 
-console.log(a);
-console.log(typeof a);
-//EoF
+// a = true; // Datentyp BOOLEAN
+// a = "Hallo!" // Datentyp STRING
+// a = 2;  // Datentyp NUMBER
+
+// console.log(a);
+// console.log(typeof a);
+// //EoF
+
+/* >>> Arrays <<<*/
+
+// let arr = new Array();
+
+let arr; 
+arr = [false,true,false,true]; // LITERALSCHREIBWEISE
+arr = ["Hallo"," ", "Du"," ","Hund","!"]
+
+console.log(arr);
+console.log(arr[0]); // ARRAY fängt bei 0 an, erste Stelle = 0
+console.log(arr.length); // LENGTH zeigt die Anzahl der Stellen des Arrays an
+console.log(arr[arr.length-1]); // letztes Element des Arrays
+console.log(arr[7]); // geht nur in JScript - geliefert wird "undefined"
