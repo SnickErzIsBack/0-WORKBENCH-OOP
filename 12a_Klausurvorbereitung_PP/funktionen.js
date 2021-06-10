5e715ea8f38a155e656f2f967c5f8d16cf801a8a
SnickErzIsBack <thiemo.neumann@web.de>
2021-06-10 08:51:01 +0200

    04_additional_info_-_NOT_test-relevant

--- 12a_Klausurvorbereitung_PP/funktionen.js
+++ 12a_Klausurvorbereitung_PP/funktionen.js
@@ -16,12 +16,24 @@
 
 // let arr = new Array();
 
-let arr; 
-arr = [false,true,false,true]; // LITERALSCHREIBWEISE
-arr = ["Hallo"," ", "Du"," ","Hund","!"]
+// let arr; 
+// arr = [false,true,false,true]; // LITERALSCHREIBWEISE
+// arr = ["Hallo"," ", "Du"," ","Hund","!"]
 
+// console.log(arr);
+// console.log(arr[0]); // ARRAY fängt bei 0 an, erste Stelle = 0
+// console.log(arr.length); // LENGTH zeigt die Anzahl der Stellen des Arrays an
+// console.log(arr[arr.length-1]); // letztes Element des Arrays
+// console.log(arr[7]); // geht nur in JScript - geliefert wird "undefined"
+
+/* Array - PLAIN INFO PART */
+let arr = [];
 console.log(arr);
-console.log(arr[0]); // ARRAY fängt bei 0 an, erste Stelle = 0
-console.log(arr.length); // LENGTH zeigt die Anzahl der Stellen des Arrays an
-console.log(arr[arr.length-1]); // letztes Element des Arrays
-console.log(arr[7]); // geht nur in JScript - geliefert wird "undefined"
+arr.push("Sau"); //Hinzufügen
+console.log(arr);
+arr.push(", du!"); //Hinzufügen IMMER an letzter Stelle
+console.log(arr);
+console.log(arr.pop()); //Entfernen - IMMER an letzter Stelle
+console.log(arr);
+arr.pop();
+console.log(arr);
