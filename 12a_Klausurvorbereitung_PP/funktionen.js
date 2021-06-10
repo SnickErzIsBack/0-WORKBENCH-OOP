0818e31e71f6e76b97f86da9a648a6ab5d613945
SnickErzIsBack <thiemo.neumann@web.de>
2021-06-10 09:33:58 +0200

    05_DOM-Klausurrelevant

--- 12a_Klausurvorbereitung_PP/funktionen.js
+++ 12a_Klausurvorbereitung_PP/funktionen.js
@@ -26,14 +26,58 @@
 // console.log(arr[arr.length-1]); // letztes Element des Arrays
 // console.log(arr[7]); // geht nur in JScript - geliefert wird "undefined"
 
-/* Array - PLAIN INFO PART */
-let arr = [];
-console.log(arr);
-arr.push("Sau"); //Hinzufügen
-console.log(arr);
-arr.push(", du!"); //Hinzufügen IMMER an letzter Stelle
-console.log(arr);
-console.log(arr.pop()); //Entfernen - IMMER an letzter Stelle
-console.log(arr);
-arr.pop();
-console.log(arr);
+//#region /* Array - PLAIN INFO PART - push/pop - FiFo -  */
+// let arr = [];
+// console.log(arr);
+// arr.push("Sau"); //Hinzufügen
+// console.log(arr);
+// arr.push(", du!"); //Hinzufügen IMMER an letzter Stelle
+// console.log(arr);
+// console.log(arr.pop()); //Entfernen - IMMER an letzter Stelle
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// // SYSTEM FIFO - first in,  first out
+//#endregion
+
+/* >>> Objekte <<<*/
+
+// let obj;
+
+// // obj = {};
+// // console.log(obj);
+
+// // obj = {x:10, y:"Hallo", z:true};
+// // console.log(obj);
+// // console.log(obj.x); // DOT-quotation, DOT-syntax
+// // console.log(obj.y);
+// // console.log(obj.z);
+
+// obj = {
+//         sub1:{
+//             sub2: {
+//                 val1: "Hi", val2: 2
+//             }
+//         }
+// }
+
+// KLAUSURRELEVANT //
+// {{{}}} ... nested object 
+// console.log(obj);
+// console.log(obj.sub1.sub2.val1);
+
+// KLAUSURRELEVANT //
+/* >>> Document Object Model - DOM - Modell der HTML-Struktur <<< */
+// steht zur Verfügung, sobald die Seite geladen und geparsed ist 
+// Strukturen Head Body
+
+// console.log(document); // <html> - Zugriff auf das gesamte DOM
+// console.log(document.body); // <body> - Zugriff auf den Body
+// console.log(document.body.children); // <body>...</body> - Zugriff auf den Array innerhalb von Body
+// console.log(document.body.children[0]); // h1 - Zugriff auf das erste Element des Arrays innerhalb von Body
+// console.log(document.body.children[0].innerHTML); // ...Text
+// document.body.children[0].innerHTML = "Hallo"; // Modifikation des Inhalts von h1
+// console.log(document.body.children[0].innerHTML);
+// console.log(document.getElementById("test")); // <p>
+// console.log(document.getElementById("test").innerHTML);
+
