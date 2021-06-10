2e3c887802d8d7eaf60fc528795d88357396122a
SnickErzIsBack <thiemo.neumann@web.de>
2021-06-10 08:32:00 +0200

    02_PRIMITIVES_and_output(data,typeof)

--- 12a_Klausurvorbereitung_PP/funktionen.js
+++ 12a_Klausurvorbereitung_PP/funktionen.js
@@ -1,3 +1,12 @@
 /* DATENSTRUKTUREN */
 
-/* Primitives */
+/* >>> Primitives <<<*/
+let a; // VAR - Deklaration - untypisierte sprache
+
+a = true; // Datentyp BOOLEAN
+a = "Hallo!" // Datentyp STRING
+a = 2;  // Datentyp NUMBER
+
+console.log(a);
+console.log(typeof a);
+//EoF
